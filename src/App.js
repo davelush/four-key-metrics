bebaf332c847f5ef7d8ee11421edadbfe9ac6822
onzo-davelush <dave.lush@onzo.com>
2019-06-10 20:54:43 +0100

    Adding React router and 3 routes / components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-        <div className="container">
-          <h2>MERN - Stack ToDo App</h2>
-        </div>
+        <router>
+            <Route path="/" exact component={TodosList} />
+            <Route path="/edit/:id" component={EditTodo} />
+            <Route path="/create" component={CreateTodo} />
+            <div className="container">
+              <h2>MERN - Stack ToDo App</h2>
+            </div>
+        </router>
     )
   };
 }
