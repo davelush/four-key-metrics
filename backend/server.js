567423ac2abfe0f620456965b22e6bea56a04202
onzo-davelush <dave.lush@onzo.com>
2019-06-12 07:57:56 +0100

    Basic backed express server with a connection to a local MongoDB

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/todos', {
+    useNewUrlParser: true
+});
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB connection established");
+});
+
+app.listen(PORT, function () {
+    console.log("Server is listening on port " + PORT);
+});
+
