856eba55245a1eabc55c7fa94011ef9c839b5318
onzo-davelush <dave.lush@onzo.com>
2019-06-12 08:21:02 +0100

    Added create, get all and get by id. Update next

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const todoRoutes = express.Router();
 const PORT = 4000;
+
+let Todo = require('./todo.model');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,9 +16,39 @@ mongoose.connect('mongodb://127.0.0.1:27017/todos', {
 });
 const connection = mongoose.connection;
 
-connection.once('open', function() {
+connection.once('open', function () {
     console.log("MongoDB connection established");
 });
+
+todoRoutes.route('/').get(function (req, res) {
+    Todo.find(function (err, todos) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(todos);
+        }
+    });
+});
+
+todoRoutes.route('/:id').get(function (req, res) {
+    let id = req.params.id;
+    Todo.findById(id, function (err, todo) {
+        res.json(todo);
+    });
+});
+
+todoRoutes.route('/add').post(function (req, res) {
+    let todo = new Todo(req.body);
+    todo.save()
+        .then(todo => {
+            res.status(200).json({'todo': 'todo added successfully'});
+        })
+        .catch(err => {
+            res.status(400).json({'todo': 'adding new todo failed'});
+        })
+});
+
+app.use('/todos', todoRoutes);
 
 app.listen(PORT, function () {
     console.log("Server is listening on port " + PORT);
