6f908bd9132d1adf66012286b51f3feacf4c2fd6
onzo-davelush <dave.lush@onzo.com>
2019-06-12 08:24:33 +0100

    And with added update

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,25 @@ todoRoutes.route('/:id').get(function (req, res) {
     });
 });
 
+
+todoRoutes.route('/update/:id').post(function(req, res) {
+    Todo.findById(req.params.id, function(err, todo) {
+        if (!todo)
+            res.status(404).send("data is not found");
+        else
+            todo.todo_description = req.body.todo_description;
+        todo.todo_responsible = req.body.todo_responsible;
+        todo.todo_priority = req.body.todo_priority;
+        todo.todo_completed = req.body.todo_completed;
+        todo.save().then(todo => {
+            res.json('Todo updated!');
+        })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+    });
+});
+
 todoRoutes.route('/add').post(function (req, res) {
     let todo = new Todo(req.body);
     todo.save()
