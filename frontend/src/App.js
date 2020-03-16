a6edbcec7e832807c66eacd82a74fe9d2388c94f
onzo-davelush <dave.lush@onzo.com>
2020-03-16 17:02:38 +0000

    Added noopener noreferred to a _blank target

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <Router>
             <div className="container">
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+                    <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank" rel="noopener noreferrer">
                         <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
                     </a>
                     <Link to="/" className="navbar-brand">MERN Stack To-Do App</Link>
