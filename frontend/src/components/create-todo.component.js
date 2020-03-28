c7274407ca1fe5cafaff01a353ecd36a1a7cb48d
onzo-davelush <dave.lush@onzo.com>
2020-03-28 08:52:37 +0000

    Integrating with backend using axios

--- frontend/src/components/create-todo.component.js
+++ frontend/src/components/create-todo.component.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class CreateTodo extends Component {
 
@@ -31,12 +32,21 @@ export default class CreateTodo extends Component {
     }
 
     onSubmit(e) {
-        e.preventDefault(e);
+        e.preventDefault();
 
         console.log(`Form submitted:`);
         console.log(`Todo Description: ${this.state.todo_description}`);
         console.log(`Todo Responsible: ${this.state.todo_responsible}`);
         console.log(`Todo Priority: ${this.state.todo_priority}`);
+
+        const newTodo = {
+            todo_description: this.state.todo_description,
+            todo_responsible: this.state.todo_responsible,
+            todo_priority: this.state.todo_priority,
+            todo_completed: this.state.todo_completed
+        };
+
+        axios.post('http://localhost:4000/todos/add', newTodo).then(res => console.log(res.data));
 
         this.setState({
             todo_description: '',
